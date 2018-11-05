65a6e246e03a3511309077349851f46dfd8968d3
Phani Prudhvi <prudhvi2050@gmail.com>
2018-11-05 15:01:49 +0530

    messagespartial

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -28,6 +28,14 @@ namespace DatingApp.API.Helpers
             CreateMap<Photo,PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto,Photo>();
             CreateMap<UserForRegisterDto,User>();
+            CreateMap<MessageForCreationDto,Message>().ReverseMap();
+            CreateMap<Message,MessageToReturnDto>()
+                .ForMember(m => m.SenderPhotoUrl, opt => {
+                    opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(u => u.IsMain).Url);
+                })
+                .ForMember(m => m.RecipientPhotoUrl, opt => {
+                    opt.MapFrom(src => src.Recipient.Photos.FirstOrDefault(u => u.IsMain).Url);
+                });
         }
     }
 }
